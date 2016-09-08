4d1e35c631a58e09410b2a00e7b23fc3e21ab8ca
Tymofii Pavliuk <pavliuk@outlook.com>
2016-09-08 16:05:59 +0300

    Tests implementation

--- src/PtNet.Utils.Linq.Tests/FirstOrDefinedTests.cs
+++ src/PtNet.Utils.Linq.Tests/FirstOrDefinedTests.cs
@@ -8,6 +8,8 @@ namespace PtNet.Utils.Linq.Tests
     [TestClass]
     public class FirstOrDefinedTests
     {
+        private readonly int[] _testArray = { 3, 10, 3, 2, 9, 10, 5, 7, 1, 4, 6, 8 };
+
         [TestMethod]
         public void FirstOrDefined_should_return_defined_value_with_empty_collection()
         {
@@ -27,7 +29,19 @@ namespace PtNet.Utils.Linq.Tests
         [TestMethod]
         public void FirstOrDefined_should_return_first_value_from_collection()
         {
-            throw new NotImplementedException();
+            var expected = 3;
+            var actual = _testArray.FirstOrDefined(77);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FirstOrDefined_should_return_first_value_from_collection_by_predicate()
+        {
+            var expected = 2;
+            var actual = _testArray.FirstOrDefined(i => i == 2, 77);
+
+            Assert.AreEqual(expected, actual);
         }
     }
 }
