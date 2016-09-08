de9150c847d434affeacd5c200625bbd250ebdc8
Tymofii Pavliuk <pavliuk@outlook.com>
2016-09-08 14:17:59 +0300

    Guard.ArgumentNotNull method

---
+++ src/PtNet.Utils.Linq.Tests/GuardTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PtNet.Utils.Linq.Tests
+{
+    [TestClass]
+    public class GuardTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void ArgumentNotNull_shold_rise_ArgumentNullException_while_argument_is_null()
+        {
+            FakeClass argument = null;
+
+            Guard.ArgumentNotNull(argument, nameof(argument));
+        }
+    }
+}
---
+++ src/PtNet.Utils/Guard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PtNet.Utils
+{
+    public static class Guard
+    {
+        public static void ArgumentNotNull(object argument, string argumentName)
+        {
+            if (argument == null)
+            {
+                throw new ArgumentNullException($"Argument {argumentName} is NULL.");
+            }
+        }
+    }
+}
