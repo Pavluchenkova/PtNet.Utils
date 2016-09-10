e65d93706463196a0764d716371ae82f538bb87f
Agnessa Pavlyuchenkova <agnessa.pavlyuchenkova@gmail.com>
2016-09-10 19:29:11 +0300

    Arguments guarded

--- src/PtNet.Utils/Linq/EnumerableExtensions.cs
+++ src/PtNet.Utils/Linq/EnumerableExtensions.cs
@@ -8,7 +8,10 @@ namespace PtNet.Utils.Linq
 	{
 		public static IEnumerable<TSource> OrderBy<TSource, TKey>(this IEnumerable<TSource> source, Func<TSource,TKey> keySelector, SortingDirection direction)
 		{
-			switch (direction)
+            Guard.ArgumentNotNull(source, nameof(source));
+            Guard.ArgumentNotNull(keySelector, nameof(keySelector));
+
+            switch (direction)
 			{
                 default:
 				case SortingDirection.Ascending:
@@ -20,11 +23,16 @@ namespace PtNet.Utils.Linq
 
         public static IEnumerable<TSource> WhereNot<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> keySelector)
 	    {
-	        return source.Where(p => !keySelector(p));
+            Guard.ArgumentNotNull(source, nameof(source));
+            Guard.ArgumentNotNull(keySelector, nameof(keySelector));
+
+            return source.Where(p => !keySelector(p));
 	    }
 
         public static TSource FirstOrDefined<TSource>(this IEnumerable<TSource> source, TSource defaultResult)
         {
+            Guard.ArgumentNotNull(source, nameof(source));
+
             var result = source.FirstOrDefault();
 
             if (EqualityComparer<TSource>.Default.Equals(result, default(TSource)))
@@ -37,6 +45,9 @@ namespace PtNet.Utils.Linq
 
         public static TSource FirstOrDefined<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> keySelector, TSource defaultResult)
         {
+            Guard.ArgumentNotNull(source, nameof(source));
+            Guard.ArgumentNotNull(keySelector, nameof(keySelector));
+            
             var result = source.FirstOrDefault(p => keySelector(p)) ;
             
             if (EqualityComparer<TSource>.Default.Equals(result, default(TSource)))
@@ -48,36 +59,52 @@ namespace PtNet.Utils.Linq
 
         public static TSource Second<TSource>(this IEnumerable<TSource> source)
         {
+            Guard.ArgumentNotNull(source, nameof(source));
+
             return source.Skip(1).First();
         }
 
         public static TSource Second<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> keySelector)
         {
+            Guard.ArgumentNotNull(source, nameof(source));
+            Guard.ArgumentNotNull(keySelector, nameof(keySelector));
+
             return source.Where(keySelector).Skip(1).First();
         }
 
         public static TSource SecondOrDefault<TSource>(this IEnumerable<TSource> source)
         {
+            Guard.ArgumentNotNull(source, nameof(source));
+
             return source.Skip(1).FirstOrDefault();
         }
 
         public static TSource SecondOrDefault<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> keySelector)
         {
+            Guard.ArgumentNotNull(source, nameof(source));
+            Guard.ArgumentNotNull(keySelector, nameof(keySelector));
+
             return source.Where(keySelector).Skip(1).FirstOrDefault();
         }
 
         public static TSource SecondOrDefined<TSource>(this IEnumerable<TSource> source, TSource defaultResult)
         {
+            Guard.ArgumentNotNull(source, nameof(source));
             return source.Skip(1).FirstOrDefined(defaultResult);    
         }
 
         public static TSource SecondOrDefined<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> keySelector, TSource defaultResult)
 	    {
+            Guard.ArgumentNotNull(source, nameof(source));
+            Guard.ArgumentNotNull(keySelector, nameof(keySelector));
+
             return source.Where(keySelector).SecondOrDefined(defaultResult);  
         }
 
         public static TSource Random<TSource>(this IEnumerable<TSource> source)
         {
+            Guard.ArgumentNotNull(source, nameof(source));
+
             var rand = new Random();
             
             return source.ElementAt(rand.Next(source.Count()));
@@ -85,12 +112,17 @@ namespace PtNet.Utils.Linq
 
         public static IEnumerable<TSource> Shuffle<TSource>(this IEnumerable<TSource> source)
         {
+            Guard.ArgumentNotNull(source, nameof(source));
+
             return source.OrderBy(elem => Guid.NewGuid());
         }
 
         public static void ForEach<TSource>(this IEnumerable<TSource> source, Action<TSource> action)
         {
-            foreach(var item in source)
+            Guard.ArgumentNotNull(source, nameof(source));
+            Guard.ArgumentNotNull(action, nameof(action));
+
+            foreach (var item in source)
             {
                 action(item);
             }
